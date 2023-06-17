645586ffe97bdab5b7266a61f96ba96e7976e170
vyacheslavsafin <safin.vr@mail.ru>
2023-06-17 20:09:41 +0300

    add func_title in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def func_upper(string):
+    """Принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return string.upper()
+
+
+def func_title(string):
+    """Делает заглавными первые буквы каждого слова в строке"""
+    return string.title()
